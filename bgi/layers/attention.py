4b2e6d06edc53becad738232835b48e98b09a497
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-05-13 02:13:09 +0200

    Add changes

--- bgi/layers/attention.py
+++ bgi/layers/attention.py
@@ -19,6 +19,8 @@ def dot_product(x, kernel):
     print(f"\n\nK backend: {K.backend()}")
     if K.backend() == 'tensorflow':
         print(tensorflow.shape(x))
+        print(tensorflow.shape(kernel))
+        print(tensorflow.shape(K.expand_dims(kernel)))
         print(tensorflow.shape(K.dot(x, K.expand_dims(kernel))))
         print(tensorflow.shape(K.squeeze(K.dot(x, K.expand_dims(kernel)), axis=-1)))
         return K.squeeze(K.dot(x, K.expand_dims(kernel)), axis=-1)
