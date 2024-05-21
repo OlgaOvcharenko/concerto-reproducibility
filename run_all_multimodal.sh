e1618dbb37d4408697c1f8930179f232751923f5
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-05-21 14:00:35 +0200

    Change # runs

--- run_all_multimodal.sh
+++ run_all_multimodal.sh
@@ -12,21 +12,22 @@ source "python_venv/bin/activate"
 # attention_s=("True False")
 # heads=("128")
 
-# epochs=("64")
+# epochs=("1")
 # lrs=("1e-5")
 # batch_sizes=("64")
 # drop_rates=("0.1 0.4")
 # attention_t=("1")
 # attention_s=("0")
 # heads=("64 128")
+# data=("simulated human")
 
 epochs=("1")
 lrs=("1e-5")
 batch_sizes=("64")
-drop_rates=("0.1 0.4")
+drop_rates=("0.1")
 attention_t=("1")
 attention_s=("0")
-heads=("64 128")
+heads=("128")
 data=("simulated human")
 
 for e in $epochs; do
