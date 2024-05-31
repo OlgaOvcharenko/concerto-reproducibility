989c295a8527f75f86c97206e752aae7ec628c64
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-06-01 00:47:18 +0200

    Add changes

--- run_multimodal_euler.sh
+++ run_multimodal_euler.sh
@@ -3,7 +3,7 @@
 #SBATCH -o logs/log-%j-multimodal.out
 #SBATCH --nodes=1
 #SBATCH --gpus=rtx_3090:1
-#SBATCH --time=16:00:00
+#SBATCH --time=32:00:00
 #SBATCH --gres=gpumem:24G
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=10G
@@ -15,3 +15,5 @@ module load gcc/8.2.0 python_gpu/3.9.9
 nvidia-smi
 
 source "python_venv/bin/activate"
+
+python3 multimodal_correct.py --epoch $1 --lr $2 --batch_size $3 --drop_rate $4 --attention_s $5 --attention_t $6 --heads $7 --data $8 --train $9
