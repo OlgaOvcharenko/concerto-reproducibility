d5c63bbf2346e22abb1f7780a46cba849f841b93
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-06-22 16:18:21 +0200

    add changes

--- run_all_multimodal_euler.sh
+++ run_all_multimodal_euler.sh
@@ -4,17 +4,17 @@ mkdir -p logs
 
 source "python_venv/bin/activate"
 
-epochs=("64")
+epochs=("150")
 lrs=("1e-3")
 batch_sizes=("64")
 drop_rates=("0.1")
 attention_t=("1")
 attention_s=("0")
-heads=("256")
+heads=("128")
 data=("simulated")
 train=1
 test=1
-model_type=("3")
+model_type=("1 2 3")
 
 for e in $epochs; do
     for lr in $lrs; do
--- run_multimodal_euler.sh
+++ run_multimodal_euler.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -o logs/log-%j-multimodal.out
 #SBATCH --nodes=1
-#SBATCH --time=10:00:00
+#SBATCH --time=24:00:00
 #SBATCH --gpus=1
 #SBATCH --gres=gpumem:23G
 #SBATCH --cpus-per-task=6
