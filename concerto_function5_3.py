f25b5c82275aaccb77acad20b853274d419788f6
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-06-23 18:25:02 +0200

    Fix

--- concerto_function5_3.py
+++ concerto_function5_3.py
@@ -1447,7 +1447,7 @@ def concerto_train_multimodal(mult_feature_names:list, RNA_tf_path: str, Protein
                                                         head_2=super_parameters["heads"],
                                                         head_3=super_parameters["heads"],
                                                         combine_omics=super_parameters['combine_omics'],
-                                                        combine_omics_model=super_parameters['model_type'])
+                                                        model_type=super_parameters['model_type'])
 
     decode_network = multi_embedding_attention_transfer(multi_max_features=[vocab_size_RNA,vocab_size_Protein],
                                                         mult_feature_names=mult_feature_names,
@@ -1458,7 +1458,7 @@ def concerto_train_multimodal(mult_feature_names:list, RNA_tf_path: str, Protein
                                                         head_2=super_parameters["heads"],
                                                         head_3=super_parameters["heads"],
                                                         combine_omics=super_parameters['combine_omics'],
-                                                        combine_omics_model=super_parameters['model_type'])
+                                                        model_type=super_parameters['model_type'])
 
     # tf_list_1 = os.listdir(os.path.join(ref_tf_path))
     tf_list_1 = [f for f in os.listdir(os.path.join(RNA_tf_path)) if 'tfrecord' in f]
