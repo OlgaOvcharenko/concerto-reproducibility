65c5770f005935018c58748b32bed8af1be4c46f
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-07-05 18:46:46 +0200

    Add changes

--- run_multimodal_euler.sh
+++ run_multimodal_euler.sh
@@ -2,17 +2,21 @@
 
 #SBATCH -o logs/log-%j-multimodal.out
 #SBATCH --nodes=1
+#SBATCH --gpus=v100:1
 #SBATCH --time=24:00:00
-#SBATCH --gpus=1
 #SBATCH --gres=gpumem:32G
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=10G
-#SBATCH --mail-type=START,END,FAIL
+#SBATCH --mail-type=END,FAIL
 
 mkdir -p logs
 
-module load gcc/8.2.0 python_gpu/3.9.9
+#module load gcc/8.2.0 python_gpu/3.9.9
+module load stack/.2024-04-silent stack/2024-04
+module load gcc/8.5.0
+module --ignore_cache load python/3.9.18
 nvidia-smi
 
 source "python_venv/bin/activate"
-python3 multimodal_correct.py --epoch $1 --lr $2 --batch_size $3 --drop_rate $4 --attention_s $5 --attention_t $6 --heads $7 --data $8 --train $9 --test ${10} --model_type ${11} --combine_omics ${12}
+
+python3 multimodal_correct.py --epoch $1 --lr $2 --batch_size $3 --drop_rate $4 --attention_s $5 --attention_t $6 --heads $7 --data $8 --train $9
