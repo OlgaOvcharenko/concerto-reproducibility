a75ddb384cb91f6782415a173fbd57c6c4110dab
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-07-05 18:22:36 +0200

    Add changes

--- setup_venv_euler.sh
+++ setup_venv_euler.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 #module load  gcc/6.3.0 python_gpu/3.7.4
-module load gcc/8.2.0 python_gpu/3.9.9
+#module load gcc/8.2.0 python_gpu/3.9.9
+module load stack/.2024-04-silent stack/2024-04
+module load gcc/8.5.0
+module --ignore_cache load python/3.9.18
 
 if [[ ! -d "python_venv" ]]; then
   echo "Create Python Virtual Environment on $HOSTNAME"
