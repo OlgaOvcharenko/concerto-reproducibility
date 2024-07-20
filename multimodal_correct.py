38461ff14f04f1c1216c1b871a13c252529e4005
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-07-20 19:44:13 +0200

    Print

--- multimodal_correct.py
+++ multimodal_correct.py
@@ -443,6 +443,7 @@ def query_to_reference(X_train, X_test, y_train, y_test):
     #     y_test[col] = y_test[col].astype('str')
 
     label_types = dict()
+    print(pd.unique(y_train))
     for i, lbl in enumerate(pd.unique(y_train)):
         label_types[lbl] = i
         y_train["ct"][y_train["ct"]==lbl] = i
