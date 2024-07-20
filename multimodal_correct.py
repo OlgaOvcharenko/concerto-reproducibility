9e34bd5fc7ff18f261d600df22dd9f76573b60fb
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-07-21 00:28:35 +0200

    Fix

--- multimodal_correct.py
+++ multimodal_correct.py
@@ -456,14 +456,17 @@ def query_to_reference(X_train, X_test, y_train, y_test):
     clusters_train = np.array(adata_new.obs["leiden"])[0:X_train.shape[0]]
     clusters_test = np.array(adata_new.obs["leiden"])[X_train.shape[0]:(X_train.shape[0]+X_test.shape[0])]
 
-    clusters_test_ix = np.ones(clusters_test.shape, dtype=int)
+    clusters_test_ix = np.zeros(clusters_test.shape, dtype=int)
     for cl in np.unique(clusters_train):
-        clusters_test_ix = clusters_test_ix & (clusters_test == cl)
+        clusters_test_ix = clusters_test_ix | (clusters_test == cl)
 
     y_predicted = np.zeros((y_test.shape[0],))
     print(clusters_test_ix)
+    print(sum(clusters_test_ix))
     print(X_test[clusters_test_ix])
     y_predicted[clusters_test_ix] = neigh.predict(X_test[clusters_test_ix])
+    print(y_test[clusters_test_ix])
+    print(y_predicted[clusters_test_ix])
     print(f"Accuracy known: {accuracy_score(y_test[clusters_test_ix], y_predicted[clusters_test_ix], normalize=False)}")
 
     y_predicted[clusters_test_ix != 1] == clusters_test[clusters_test_ix != 1]
