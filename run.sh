bde7990a2b615f3cf50f6612bd3dd5fef0ed89ad
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-08-05 01:32:53 +0200

    Add plots run

--- run.sh
+++ run.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-#SBATCH -o logs/log-%j.out
-#SBATCH --nodes=1
-#SBATCH --gpus=rtx_3090:1
-#SBATCH --time=04:00:00
-#SBATCH --gres=gpumem:24G
-#SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=10G
+python make_plot.py --epoch 150 --lr 0.001 --batch_size 64 --drop_rate 0.1 --attention_s 0 --attention_t 1 --heads 16 --data simulated --train 0 --test 1 --model_type 5 --combine_omics 0 > simulated_test_0_mt_5_16_plots.out 2>&1 &
 
-mkdir -p logs
+python make_plot.py --epoch 150 --lr 0.001 --batch_size 64 --drop_rate 0.1 --attention_s 0 --attention_t 1 --heads 16 --data simulated --train 0 --test 1 --model_type 4 --combine_omics 0 > simulated_test_0_mt_4_16_plots.out 2>&1 &
 
-module load gcc/8.2.0 python_gpu/3.9.9
-nvidia-smi
+python make_plot.py --epoch 150 --lr 0.001 --batch_size 64 --drop_rate 0.1 --attention_s 0 --attention_t 1 --heads 16 --data simulated --train 0 --test 1 --model_type 3 --combine_omics 0 > simulated_test_0_mt_3_16_plots.out 2>&1 &
 
-source "python_venv/bin/activate"
+python make_plot.py --epoch 150 --lr 0.001 --batch_size 64 --drop_rate 0.1 --attention_s 0 --attention_t 1 --heads 16 --data simulated --train 0 --test 1 --model_type 2 --combine_omics 0 > simulated_test_0_mt_2_16_plots.out 2>&1 &
 
-python3 $1
+python make_plot.py --epoch 150 --lr 0.001 --batch_size 64 --drop_rate 0.1 --attention_s 0 --attention_t 1 --heads 16 --data simulated --train 0 --test 1 --model_type 1 --combine_omics 0 > simulated_test_0_mt_1_16_plots.out 2>&1 &
 
+wait
