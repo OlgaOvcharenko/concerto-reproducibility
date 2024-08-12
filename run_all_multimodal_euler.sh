cb2fdaccf22e3c82744e42d7f2f6365c325c5aeb
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-08-12 22:48:10 +0200

    Modify run

--- run_all_multimodal_euler.sh
+++ run_all_multimodal_euler.sh
@@ -4,20 +4,6 @@ mkdir -p logs
 
 source "python_venv/bin/activate"
 
-# epochs=("200")
-# lrs=("1e-3")
-# batch_sizes=("64")
-# drop_rates=("0.1")
-# attention_t=("1")
-# attention_s=("0")
-# heads=("16")
-# data=("simulated")
-# train=1
-# test=1
-
-# model_type=("0")
-# combine_omics=1
-
 epochs=("200")
 lrs=("1e-3")
 batch_sizes=("64")
@@ -25,8 +11,8 @@ drop_rates=("0.1")
 attention_t=("1")
 attention_s=("0")
 heads=("16")
-data=("human")
-train=1
+data=("simulated")
+train=0
 test=1
 
 model_type=("1 2 3 4 5")
@@ -52,25 +38,25 @@ for e in $epochs; do
     done 
 done
 
-# model_type=("0")
-# combine_omics=1
+model_type=("0")
+combine_omics=1
 
-# for e in $epochs; do
-#     for lr in $lrs; do
-#         for batch_size in $batch_sizes; do
-#             for drop_rate in $drop_rates; do
-#                 for s in $attention_s; do
-#                     for t in $attention_t; do
-#                         for h in $heads; do
-#                             for d in $data; do
-#                                 for mt in $model_type; do
-#                                     sbatch run_multimodal_euler.sh $e $lr $batch_size $drop_rate $s $t $h $d $train $test $mt $combine_omics
-#                                 done
-#                             done
-#                         done
-#                     done
-#                 done
-#             done
-#         done
-#     done 
-# done
+for e in $epochs; do
+    for lr in $lrs; do
+        for batch_size in $batch_sizes; do
+            for drop_rate in $drop_rates; do
+                for s in $attention_s; do
+                    for t in $attention_t; do
+                        for h in $heads; do
+                            for d in $data; do
+                                for mt in $model_type; do
+                                    sbatch run_multimodal_euler.sh $e $lr $batch_size $drop_rate $s $t $h $d $train $test $mt $combine_omics
+                                done
+                            done
+                        done
+                    done
+                done
+            done
+        done
+    done 
+done
