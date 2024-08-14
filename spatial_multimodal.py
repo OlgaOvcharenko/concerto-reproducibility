27560971eff80ffb6c3fc43bfcebf1e226d062be
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-08-14 17:52:28 +0200

    Try

--- spatial_multimodal.py
+++ spatial_multimodal.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-# from tensorflow.python.client import device_lib
+from tensorflow.python.client import device_lib
 
 def get_args():
     parser = argparse.ArgumentParser(description='CONCERTO Batch Correction.')
@@ -115,12 +115,14 @@ def main():
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
 
-    exit()
-    # def get_available_devices():
-    #     local_device_protos = device_lib.list_local_devices()
-    #     return [x.name for x in local_device_protos]
+    
+    def get_available_devices():
+        local_device_protos = device_lib.list_local_devices()
+        return [x.name for x in local_device_protos]
 
-    # print(get_available_devices())
+    print(get_available_devices())
+
+    exit()
 
     # Read data
     save_path = './Multimodal_pretraining/'
