0d3b1dd66e70a7a186c8f69927fe959546e236ca
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-08-16 01:03:28 +0200

    Add experiment

--- run_all_multimodal_euler.sh
+++ run_all_multimodal_euler.sh
@@ -15,31 +15,8 @@ data=("human_cite human_cite_raw")
 train=1
 test=1
 
-# model_type=("1 2 3 4 5")
-# combine_omics=0
-
-# for e in $epochs; do
-#     for lr in $lrs; do
-#         for batch_size in $batch_sizes; do
-#             for drop_rate in $drop_rates; do
-#                 for s in $attention_s; do
-#                     for t in $attention_t; do
-#                         for h in $heads; do
-#                             for d in $data; do
-#                                 for mt in $model_type; do
-#                                     sbatch run_multimodal_euler.sh $e $lr $batch_size $drop_rate $s $t $h $d $train $test $mt $combine_omics
-#                                 done
-#                             done
-#                         done
-#                     done
-#                 done
-#             done
-#         done
-#     done 
-# done
-
-model_type=("0")
-combine_omics=1
+model_type=("1 2 3 4 5")
+combine_omics=0
 
 for e in $epochs; do
     for lr in $lrs; do
@@ -60,3 +37,26 @@ for e in $epochs; do
         done
     done 
 done
+
+# model_type=("0")
+# combine_omics=1
+
+# for e in $epochs; do
+#     for lr in $lrs; do
+#         for batch_size in $batch_sizes; do
+#             for drop_rate in $drop_rates; do
+#                 for s in $attention_s; do
+#                     for t in $attention_t; do
+#                         for h in $heads; do
+#                             for d in $data; do
+#                                 for mt in $model_type; do
+#                                     sbatch run_multimodal_euler.sh $e $lr $batch_size $drop_rate $s $t $h $d $train $test $mt $combine_omics
+#                                 done
+#                             done
+#                         done
+#                     done
+#                 done
+#             done
+#         done
+#     done 
+# done
