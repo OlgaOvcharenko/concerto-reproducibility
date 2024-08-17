c5e76e196b95af3ec840c9e395bb49c8ebc9c784
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-08-18 01:06:34 +0200

    Fix array

--- spatial_multimodal.py
+++ spatial_multimodal.py
@@ -93,7 +93,7 @@ def test_concerto(adata_RNA, weight_path: str, data: str,
     # Test
     nn = "encoder"
     dr = 0.0 # drop_rate
-    only_images = True # [True, False] # if combine_omics == 0 else [False]
+    only_images = [True] # [True, False] # if combine_omics == 0 else [False]
     for only_image in only_images:
         for e in [4, 8]: 
             saved_weight_path = f'./Multimodal_pretraining/weight/multi_weight_{nn}_{data}_{mask}_{batch_size}_model_{combine_omics}_{model_type}_epoch_{e}_{lr}_{drop_rate}_{attention_t}_{attention_s}_{heads}.h5'
