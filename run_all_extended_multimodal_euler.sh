a3986af0ad302432c4d923d31fa82b376b6c64ea
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-09-17 16:52:32 +0200

    Add runs

---
+++ run_all_extended_multimodal_euler.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+mkdir -p logs
+
+source "python_venv/bin/activate"
+
+batch_sizes=("64")
+drop_rates=("0.1")
+attention_t=("1")
+attention_s=("0")
+heads=("128")
+train=1
+test=0
+
+epochs=("3")
+lrs=("1e-3")
+data=("simulated human_cite")
+model_type=("0")
+combine_omics=1
+tasks=("0 1")
+
+
+for e in $epochs; do
+    for lr in $lrs; do
+        for batch_size in $batch_sizes; do
+            for drop_rate in $drop_rates; do
+                for s in $attention_s; do
+                    for t in $attention_t; do
+                        for h in $heads; do
+                            for d in $data; do
+                                for mt in $model_type; do
+                                    for task in $tasks; do
+                                        sbatch run_extended_multimodal_euler.sh $e $lr $batch_size $drop_rate $s $t $h $d $train $test $mt $combine_omics $task
+                                    done
+                                done
+                            done
+                        done
+                    done
+                done
+            done
+        done
+    done 
+done
