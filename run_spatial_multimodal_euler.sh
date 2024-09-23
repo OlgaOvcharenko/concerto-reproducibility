760228fc8b4fd3ff2b01fa5e1e4ea26e13a7e2d2
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-09-23 23:35:07 +0200

    Change setup

--- run_spatial_multimodal_euler.sh
+++ run_spatial_multimodal_euler.sh
@@ -3,10 +3,10 @@
 #SBATCH -o logs/log-%j-multimodal.out
 #SBATCH --nodes=1
 #SBATCH --gpus=1
-#SBATCH --time=10:00:00
+#SBATCH --time=12:00:00
 #SBATCH --gres=gpumem:21G
 #SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=21G
+#SBATCH --mem-per-cpu=12G
 #SBATCH --mail-type=END,FAIL
 
 mkdir -p logs
