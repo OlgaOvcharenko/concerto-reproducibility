2069de49814e78ce2f2c8ab1f9a8c26dadd2769e
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-09-29 01:27:42 +0200

    No imagenet

--- bgi/models/DeepSingleCell.py
+++ bgi/models/DeepSingleCell.py
@@ -143,7 +143,7 @@ def make_spatial_RNA_image_model(multi_max_features: list = [40000],
             base_model2 = EfficientNetB7(
                 input_shape=(multi_max_features[1], multi_max_features[1], 3),
                 include_top=False,
-                weights='model_weights/efficientnetb7.h5',
+                weights=None #'model_weights/efficientnetb7.h5',
             )
             base_model2.build()
             base_model2.layers.pop()
@@ -179,9 +179,8 @@ def make_spatial_RNA_image_model(multi_max_features: list = [40000],
             base_model2 = EfficientNetB4(
                 input_shape=(multi_max_features[1], multi_max_features[1], 3),
                 include_top=False,
-                weights='model_weights/efficientnetb4.h5',
+                weights=None #'model_weights/efficientnetb4.h5',
             )
-            base_model2.build()
             base_model2.layers.pop()
             base_model = Model(base_model2.input, base_model2.layers[-1].output)
             
