56975bbdccbbc38e7eb1276b86e8460cc0ec3be0
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-10-03 20:16:16 +0200

    Add changes

--- run_all_spatial_multimodal_euler.sh
+++ run_all_spatial_multimodal_euler.sh
@@ -4,7 +4,7 @@ mkdir -p logs
 
 source "python_venv/bin/activate"
 
-epochs=("100")
+epochs=("54")
 lrs=("1e-5")
 batch_sizes=("64")
 drop_rates=("0.1")
@@ -12,7 +12,7 @@ attention_t=("1")
 attention_s=("0")
 heads=("128")
 data=("spatial_split")
-train=1
+train=0
 test=1
 mask=("0 1")
 
