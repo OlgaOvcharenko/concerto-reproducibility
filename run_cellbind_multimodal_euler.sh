fbd6d590c3daf2d278b44ec332d75788d290e8a3
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-10-03 19:42:25 +0200

    Changes req

--- run_cellbind_multimodal_euler.sh
+++ run_cellbind_multimodal_euler.sh
@@ -2,9 +2,7 @@
 
 #SBATCH -o logs/log-%j-multimodal.out
 #SBATCH --nodes=1
-#SBATCH --gpus=1
-#SBATCH --time=10:00:00
-#SBATCH --gres=gpumem:21G
+#SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=8G
 #SBATCH --mail-type=END,FAIL
