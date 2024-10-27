225e399c4742e04af7424d665ded296c7fc41853
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-10-27 15:20:02 +0100

    Add file

---
+++ metrics_new.py
@@ -0,0 +1,81 @@
+import argparse
+from scib_metrics.benchmark import Benchmarker, BioConservation, BatchCorrection
+import scanpy as sc
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+from os import listdir
+from os.path import isfile, join
+
+
+_BIO_METRICS = BioConservation(isolated_labels=True, 
+                               nmi_ari_cluster_labels_leiden=True, 
+                               nmi_ari_cluster_labels_kmeans=False, 
+                               silhouette_label=True, 
+                               clisi_knn=True
+                               )
+_BATCH_METRICS = BatchCorrection(graph_connectivity=True, 
+                                 kbet_per_label=True, 
+                                 ilisi_knn=True, 
+                                 pcr_comparison=True, 
+                                 silhouette_batch=True
+                                 )
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='CONCERTO Batch Correction.')
+
+    parser.add_argument('--data', type=str, required=True,
+                        help='Adata path')
+
+    args = parser.parse_args()
+    return args
+
+
+def evaluate_model(adata, batch_key="batch", cell_type_label="cell_type_l1"):
+    names_obs = list(adata.obsm.keys())
+    names_obs.remove("X_pca")
+    names_obs.remove("Unintegrated_HVG_only")
+    print(names_obs)
+    bm = Benchmarker(
+                adata,
+                batch_key=batch_key,
+                label_key=cell_type_label,
+                embedding_obsm_keys=names_obs,
+                bio_conservation_metrics=_BIO_METRICS,
+                batch_correction_metrics=_BATCH_METRICS,
+                n_jobs=4,
+            )
+    bm.benchmark()
+    a = bm.get_results(False, True)
+    print(a)
+    results = a.round(decimals=4)
+    # results = a[:1].astype(float).round(4)
+    return results
+
+args = get_args()
+data = args.data
+repeat = 0
+
+print("Read adata")
+adata = None
+
+
+only_files = [f for f in listdir(f'./Multimodal_pretraining/data/{data}/') if isfile(join(f'./Multimodal_pretraining/data/{data}/', f)) if f.startswith(f'Multimodal_pretraining/data/{data}/{data}_bc_')]
+print(only_files)
+exit()
+
+
+path = filename = f'./Multimodal_pretraining/data/{data}/{data}_bc_{combine_omics}_mt_{model_type}_bs_{batch_size}_{epoch}_{lr}_{drop_rate}_0_1_{heads}_{repeat}.h5ad'
+file_repeat = data.split(".h5ad")[0][:-1] + f"{repeat}.h5ad"
+if repeat == 0:
+    adata = sc.read_h5ad(file_repeat) 
+else:
+    print("here")
+    tmp = sc.read_h5ad(file_repeat) 
+    names_obs = list(tmp.obsm.keys())
+    names_obs.remove("X_pca")
+    names_obs.remove("Unintegrated_HVG_only")
+    adata.obsm[f"Embedding_{repeat}"] = tmp.obsm[names_obs[0]]
+
+df = evaluate_model(adata=adata)
+df.to_csv(f'./Multimodal_pretraining/results/{data.split("/")[-1][:-5].split("_")[0]}/{data.split("/")[-1][:-5]}_metrics_True.csv')
