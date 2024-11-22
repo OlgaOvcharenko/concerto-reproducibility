383a1f14d8d40ae3119dc76f91cc5fad13fbd58b
OlgaOvcharenko <ovcharenko.folga@gmail.com>
2024-11-22 02:44:01 +0100

    Add svgs

--- spatial_multimodal.py
+++ spatial_multimodal.py
@@ -196,6 +196,7 @@ def test_concerto(adata_RNA, weight_path: str, data: str,
             # sc.set_figure_params(dpi=150)
             sc.pl.umap(adata_RNA_1, color=color, size=10, legend_fontweight='light') # edges=True
             plt.savefig(f'./Multimodal_pretraining/plots/{data}/{data}_{mask}_{"train" if train else "test"}_{combine_omics}_oRNA{only_image}_mt_{model_type}_bs_{batch_size}_{nn}_{e}_{lr}_{drop_rate}_{dr}_{attention_s}_{attention_t}_{heads}.svg')
+            plt.savefig(f'./Multimodal_pretraining/plots/{data}/{data}_{mask}_{"train" if train else "test"}_{combine_omics}_oRNA{only_image}_mt_{model_type}_bs_{batch_size}_{nn}_{e}_{lr}_{drop_rate}_{dr}_{attention_s}_{attention_t}_{heads}.png')
 
     return adata_merged
 
@@ -302,6 +303,7 @@ def test_concerto_full(adata_RNA, adata_RNA_test, weight_path: str, data: str,
                 sc.pp.neighbors(adata_RNA_1, use_rep="X_embedding", metric="cosine")
                 sc.tl.umap(adata_RNA_1, min_dist=0.1)
                 sc.pl.umap(adata_RNA_1, color=[cell_labels], size=10, legend_fontweight='light') # edges=True
+                plt.savefig(f'./Multimodal_pretraining/plots/{data}/{data}_{mask}_train_{combine_omics}_oRNA{only_image}_mt_{model_type}_bs_{batch_size}_{nn}_{e}_{lr}_{drop_rate}_{dr}_{attention_s}_{attention_t}_{heads}.svg')
                 plt.savefig(f'./Multimodal_pretraining/plots/{data}/{data}_{mask}_train_{combine_omics}_oRNA{only_image}_mt_{model_type}_bs_{batch_size}_{nn}_{e}_{lr}_{drop_rate}_{dr}_{attention_s}_{attention_t}_{heads}.png')
 
                 adata_RNA_1_test = adata_RNA_test[RNA_id_test].copy()
@@ -310,6 +312,7 @@ def test_concerto_full(adata_RNA, adata_RNA_test, weight_path: str, data: str,
                 sc.pp.neighbors(adata_RNA_1_test, use_rep="X_embedding", metric="cosine")
                 sc.tl.umap(adata_RNA_1_test, min_dist=0.1)
                 sc.pl.umap(adata_RNA_1_test, color=[cell_labels, 'pred_cell_type'], size=10, legend_fontweight='light') # edges=True
+                plt.savefig(f'./Multimodal_pretraining/plots/{data}/{data}_{mask}_test_{combine_omics}_oRNA{only_image}_{modality}_mt_{model_type}_bs_{batch_size}_{nn}_{e}_{lr}_{drop_rate}_{dr}_{attention_s}_{attention_t}_{heads}.svg')
                 plt.savefig(f'./Multimodal_pretraining/plots/{data}/{data}_{mask}_test_{combine_omics}_oRNA{only_image}_{modality}_mt_{model_type}_bs_{batch_size}_{nn}_{e}_{lr}_{drop_rate}_{dr}_{attention_s}_{attention_t}_{heads}.png')
 
     return res_df
